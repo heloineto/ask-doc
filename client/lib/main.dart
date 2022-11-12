aeee044ffeb1fbea0fde5da1715ff751e0b5d721
VytorLelis <vytorhugo1@hotmail.com>
2022-11-12 20:37:48 -0300

    Co-authored-by: Heloi Neto <hey@heloineto.com>

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:client/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tailwind_colors/tailwind_colors.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ask Doc',
-      debugShowCheckedModeBanner: false,
-      routes: routes,
-      theme: ThemeData.dark().copyWith(
-        primaryColor: TW3Colors.green,
-        textTheme: GoogleFonts.interTextTheme(),
+    return ChangeNotifierProvider(
+      // create: () =>  ,
+      child: MaterialApp(
+        title: 'Ask Doc',
+        debugShowCheckedModeBanner: false,
+        routes: routes,
+        theme: ThemeData.dark().copyWith(
+          primaryColor: TW3Colors.green,
+          textTheme: GoogleFonts.interTextTheme(),
+        ),
       ),
     );
   }
